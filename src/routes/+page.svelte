cf0aa7090ff47258aab31645ed9a482576990f38
samalaksa <salaksa@confederationcollege.ca>
2024-11-20 16:04:58 -0500

    added to do list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Todo from './Todo.svelte';
+
+</script>
+
+<h1>Self Care To Do App</h1>
+
+<Todo />
