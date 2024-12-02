9ceb71123289642f4164ae46d168aab17cfdecf4
samalaksa <salaksa@confederationcollege.ca>
2024-12-01 20:47:48 -0500

    add underline

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script>
     import Todo from './Todo.svelte';
     import '../app.css';
-    import frame from "$lib/img/frame.png"
+    import underline from "$lib/img/underline.png"
 </script>
 
     <h2>Self Care</h2>
+    <img class="underline" src="{underline}" alt="trash"/>
     <h1>TO DO LIST</h1>
 
 <Todo />
 
 <style>
+    .underline {
+        width: 35%;
+        opacity: 30%;
+    }
 </style>
